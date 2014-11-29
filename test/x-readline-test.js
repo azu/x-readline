e63bb27a7c33aabc1415a813cd1a4256b9a7af01
azu <azuciao@gmail.com>
2014-11-29 18:42:57 +0900

    initial commit

---
+++ test/x-readline-test.js
@@ -0,0 +1,23 @@
+// LICENSE : MIT
+"use strict";
+
+var assert = require("power-assert");
+var XReadLine = require('../lib/x-readline');
+var fs = require('fs');
+var rs = fs.createReadStream(__dirname + '/fixtures/test.txt', {encoding: 'utf8'});
+describe("x-readline-test", function () {
+    it("read by x-line", function (done) {
+        var xReadLine = new XReadLine(rs);
+        var results = [];
+        xReadLine.forEach(3, function (list) {
+            results.push(list);
+        }, function () {
+            assert.deepEqual(results, [
+                [1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]
+            ]);
+            done()
+        });
+    });
+});
